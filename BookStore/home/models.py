db85bc1cac8fef02bf846549ead6ad11b0f16084
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-27 20:18:15 +0300

    add models in admin and crete list filter for book model

--- BookStore/home/models.py
+++ BookStore/home/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
+import uuid
+
+
+class Isbn(models.Model):
+    author_title = models.CharField(max_length=30)
+    book_title = models.CharField(max_length=30)
+    isbn = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+    def __str__(self):
+        return self.author_title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -9,9 +27,15 @@ class Book(models.Model):
     description = models.TextField()
     rate = models.IntegerField()
     views = models.IntegerField()
-    slug = models.SlugField(unique=True, max_length=255, blank=True)
+    slug = models.SlugField(unique=True, max_length=255, blank=True , editable=False)
+    user = models.ForeignKey(User , on_delete=models.PROTECT)
+    isbn = models.OneToOneField(Isbn , on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super(Book, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.author_name
