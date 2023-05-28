d130af0090d77c4015116bf2618f521278d0d5fb
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-28 21:26:46 +0300

    create forms for login and signup

--- BookStore/user/urls.py
+++ BookStore/user/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "BookStore"
 
 urlpatterns = [
-    path("login" , views.user_login , name="user-login"),
-    path("logout" , views.user_logout , name="user-logout"),
-    path("signup" , views.user_signup , name="user-signup"),
+    path("login" , views.user_login , name="login"),
+    path("logout" , views.user_logout , name="logout"),
+    path("signup" , views.user_signup , name="signup"),
 ]
--- BookStore/user/views.py
+++ BookStore/user/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render , redirect
 from django.contrib import messages
 from django.contrib.auth import login , logout , authenticate
 from django.contrib.auth.decorators import login_required
+from .forms import LoginForm , SignUpForm
 
 
 def user_login(request):
+    form = LoginForm()
     if request.user.is_authenticated:
         return redirect("BookStore:home-index")
 
@@ -13,15 +15,20 @@ def user_login(request):
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is None:
-            return render(request, "auth/login.html", messages)
+            return render(request, "accounts/login.html", {"form": form})
         return redirect("BookStore:home-index")
     else:
-        return render(request, "auth/login.html")
+        return render(request, "accounts/login.html" , {"form": form})
 
 
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect("login")
 
 def user_signup(request):
-    pass
+    form = SignUpForm()
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "accounts/signup.html" , {"form": form})
