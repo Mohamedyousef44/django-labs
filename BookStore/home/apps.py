d3ca9b02166300d4ba3ff3c64eae854cc1f0eddb
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-28 19:25:19 +0300

    create auth app

--- BookStore/home/apps.py
+++ BookStore/home/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+    # def ready():     // used for register signals because it is invoked when app init
+    #     pass
