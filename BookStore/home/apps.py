d625b8e62f744c9c9a6ffa1835d7540df22e52d9
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-28 23:16:44 +0300

    cteate signal

--- BookStore/home/apps.py
+++ BookStore/home/apps.py
@@ -5,5 +5,5 @@ class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
 
-    # def ready():     // used for register signals because it is invoked when app init
-    #     pass
+    def ready(self):     # used for register signals because it is invoked when app init
+        from .signals import create_and_assign_isbn_to
